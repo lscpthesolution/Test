eeceeffe4137e7169f7477f52bcec60c01c69cc3
Minhyuk Adam Oh <adam.oh@libertas-scholars.orgclera>
2024-10-31 08:32:11 +0900

    triangle checker

---
+++ Sample04.py
@@ -0,0 +1,16 @@
+target = 25
+
+for each in range(target, -1, -1):
+    try:
+        print(target/each)
+    except:
+        print("Error")
+
+
+
+
+################ check if it is a triangle ###############
+triangle = [(3,3,3),(4,5,2),(1,2,5)]
+
+def checker():
+    #show your work here
