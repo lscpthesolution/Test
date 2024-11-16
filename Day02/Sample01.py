d3a91fb092022fb16c951c627191707ad816a84f
Minhyuk Adam Oh <adam.oh@libertas-scholars.orgclera>
2024-11-16 23:26:05 +0900

    updated

--- Day02/Sample01.py
+++ Day02/Sample01.py
@@ -1,22 +1,45 @@
-'''
-Return a list consists of five different numbers from the user
-'''
-def makeFunc() -> list:
-    result = list()
-    for each in range(5):
-        num = input("Enter any number : ")
-        result.append(num)
+# class -> function -> variable
+# class.variable            X
+# class.function            O
+# function.variable         O
 
-    return result
+class Student:
+    def __init__(self, name:str, id:str) -> None:
+        self.__name = name
+        self.__id = id
+
+    # getter
+    '''
+    return the class variable, __name
+    '''
+    def getName(self) -> str:
+        return self.__name
+
+    # Setter
+
+    '''
+    set a new student ID and return None
+    '''
+    def setID(self, givenID:str) -> None:
+        self.setID = givenID
 
 
-'''
-return the sum of the two given numbers
-'''
-def adder(first:int, second:int) -> None:
-    print(first + second)
+    '''
+    set a new name and return None
+    '''
+    def setName(self, name:str) -> None:
+        self.__name = name
 
-######################  RUN ###########################################3
-# print(makeFunc())
 
-adder(10, 9)
+
+s01 = Student("Adam", "123123")
+s02 = Student("Seon", "125489")
+
+# print(s01.__name)
+# print(s02.__name)
+# print(s01.__id)
+# s01.__id = "11111111"
+s01.setID("1111111")
+print(s01.getName())
+s02.setName("Now")
+print(s02.getName())
